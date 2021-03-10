e18d1357224349b3d224881ae9669833a1b35ba4
evgeniy-web-dev <vem-89@mail.ru>
2021-03-10 04:08:09 +0300

    feat: implements towel sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 
-// You should implement your task here.
+// Алгоритм «сортировки полотенец»
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    let result = [];
+    if (matrix === undefined){return result};
+    for (i = 0; i<matrix.length; i++){
+      if (i % 2 === 0){
+      for (n = 0; n<matrix[i].length; n++ ){
+        result.push(matrix[i][n])
+      }
+      }
+      if (i % 2 !==0){
+        for (z = matrix[i].length-1; z>=0; z-- ){
+            result.push(matrix[i][z])
+  
+      }
+      }
+        }
+    return(result)
+  }
